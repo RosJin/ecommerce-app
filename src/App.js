260a69fcb0b0aefbd75120870d6d213b38c0a3d9
RosJin <huyhoang.hn462@gmail.com>
2023-09-15 17:42:00 +0700

    policy

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ import Forgotpassword from "./pages/Forgotpassword";
 import Signup from "./pages/Signup";
 import Resetpassword from "./pages/Resetpassword";
 import SingleBlog from "./pages/SingleBlog";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import RefundPolicy from "./pages/RefundPolicy";
+import ShippingPolicy from "./pages/ShippingPolicy";
+import TermAndContions from "./pages/TermAndContions";
 
 function App() {
     return (
@@ -33,6 +37,11 @@ function App() {
                         <Route path="forgot-password" element={<Forgotpassword />} />
                         <Route path="signup" element={<Signup />} />
                         <Route path="reset-password" element={<Resetpassword />} />
+                        <Route path="privacy-policy" element={<PrivacyPolicy />} />
+                        <Route path="refund-policy" element={<RefundPolicy />} />
+                        <Route path="shipping-policy" element={<ShippingPolicy />} />
+                        <Route path="term-conditions" element={<TermAndContions />} />
+                        
                     </Route>
                 </Routes>
             </BrowserRouter>
