b553ccb07ad18614694047c6d2ae9654072c08f4
RosJin <huyhoang.hn462@gmail.com>
2023-09-17 19:11:28 +0700

    final cart page

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -17,9 +17,9 @@ const ProductCard = (props) => {
             <div className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}>
                 <Link to=":id" className="product-card position-relative">
                     <div className="wishlist-icon position-absolute">
-                        <Link>
+                        <button className="border-0 bg-transparent">
                             <img src={wish} alt="wishlist" />
-                        </Link>
+                        </button>
                     </div>
                     <div className="product-image">
                         <img src={watch} className="img-fluid" alt="product imag" />
@@ -43,15 +43,15 @@ const ProductCard = (props) => {
                     </div>
                     <div className="action-bar position-absolute">
                         <div className="d-flex flex-column gap-15">
-                            <Link>
+                            <button className="border-0 bg-transparent">
                                 <img src={prodcompare} alt="compare" />
-                            </Link>
-                            <Link>
+                            </button>
+                            <button className="border-0 bg-transparent">
                                 <img src={view} alt="view" />
-                            </Link>
-                            <Link>
+                            </button>
+                            <button className="border-0 bg-transparent">
                                 <img src={addcart} alt="add cart" />
-                            </Link>
+                            </button>
                         </div>
                     </div>
                 </Link>
@@ -59,9 +59,9 @@ const ProductCard = (props) => {
             <div className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}>
                 <Link className="product-card position-relative">
                     <div className="wishlist-icon position-absolute">
-                        <Link>
+                        <button className="border-0 bg-transparent">
                             <img src={wish} alt="wishlist" />
-                        </Link>
+                        </button>
                     </div>
                     <div className="product-image">
                         <img src={watch} className="img-fluid" alt="product imag" />
@@ -85,15 +85,15 @@ const ProductCard = (props) => {
                     </div>
                     <div className="action-bar position-absolute">
                         <div className="d-flex flex-column gap-15">
-                            <Link>
+                            <button className="border-0 bg-transparent">
                                 <img src={prodcompare} alt="compare" />
-                            </Link>
-                            <Link>
+                            </button>
+                            <button className="border-0 bg-transparent">
                                 <img src={view} alt="view" />
-                            </Link>
-                            <Link>
+                            </button>
+                            <button className="border-0 bg-transparent">
                                 <img src={addcart} alt="add cart" />
-                            </Link>
+                            </button>
                         </div>
                     </div>
                 </Link>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,6 +3,7 @@ import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import watch from "../images/watch.jpg";
 import { AiFillDelete } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
     return (
@@ -26,8 +27,8 @@ const Cart = () => {
                                     </div>
                                     <div className="w-75">
                                         <p>asdasd</p>
-                                        <p>dasd</p>
-                                        <p>sada</p>
+                                        <p>Size: dasd</p>
+                                        <p>Color: sada</p>
                                     </div>
                                 </div>
                                 <div className="cart-col-2">
@@ -45,30 +46,18 @@ const Cart = () => {
                                     <h5 className="price">$ 100</h5>
                                 </div>
                             </div>
-                            <div className="cart-data py-3 mb-2 d-flex justify-content-between align-items-center">
-                                <div className="cart-col-1 gap-15 d-flex align-items-center">
-                                    <div className="w-25">
-                                        <img src={watch} className="img-fluid" alt="product img" />
-                                    </div>
-                                    <div className="w-75">
-                                        <p>asdasd</p>
-                                        <p>dasd</p>
-                                        <p>sada</p>
-                                    </div>
-                                </div>
-                                <div className="cart-col-2">
-                                    <h5 className="price">$ 100</h5>
-                                </div>
-                                <div className="cart-col-3 d-flex align-items-center gap-15">
-                                    <div>
-                                        <input className="form-control" type="number" name="" id="" min={1} max={999} />
-                                    </div>
-                                    <div>
-                                        <AiFillDelete className="text-danger" />
-                                    </div>
-                                </div>
-                                <div className="cart-col-4">
-                                    <h5 className="price">$ 100</h5>
+                        </div>
+                        <div className="col-12 py-2 mt-4">
+                            <div className="d-flex justify-content-between align-items-baseline">
+                                <Link to="/product" className="button">
+                                    Continue To Shopping
+                                </Link>
+                                <div className="d-flex flex-column align-items-end">
+                                    <h4>SubTotal : $ 100</h4>
+                                    <p>Taxes and shipping calculated at checkout</p>
+                                    <Link to="/checkout" className="button">
+                                        Checkout
+                                    </Link>
                                 </div>
                             </div>
                         </div>
